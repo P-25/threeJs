6439e1b73a17ed4c5ac682f6fd27f68a9fa38f08
Prince Sharma <prince@appycodes.com>
2024-08-07 12:58:28 +0530

    feat: added lobby background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,65 @@
-export default function Index() {
+import { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "@/components/Loader";
+
+import Prince from "@/models/prince";
+import Lobby from "@/models/lobby";
+
+export default function Home() {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
+  // const [showPopup, setShowPopup] = useState(false);
+
+  // const handleConfirm = () => {
+  //   setShowPopup(false);
+  //   console.log("Confirmed!", showPopup);
+  // };
+
+  const adjustIs1andForScreenSize = () => {
+    let screenSca1e = null;
+    const screenPosition = [40, -30, -50];
+    const rotation = [0, 5, 0];
+    if (typeof window !== "undefined") {
+      if (window.innerWidth < 768) {
+        screenSca1e = [10, 10, 10];
+      } else {
+        screenSca1e = [20, 20, 20];
+      }
+    }
+    return [screenSca1e, screenPosition, rotation];
+  };
+
+  const [islandSca1e, islandPosition, islandRotation] =
+    adjustIs1andForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
-      <div
-        className="relative h-screen w-full flex items-center justify-center bg-cover bg-center text-center px-5"
-        style={{
-          backgroundImage:
-            "url(https://images.pexels.com/photos/601174/pexels-photo-601174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
-        }}
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
-
-        <div className="z-50 flex flex-col justify-center text-white w-full h-screen">
-          <h1 className="text-5xl">
-            <b>Almost</b> there!
-          </h1>
-          <br></br>
-          <p>Stay tuned for something amazing!!!</p>
-
-          <div className="mt-10 mb-5">
-            <div className="shadow w-full bg-white mt-2 max-w-2xl mx-auto rounded-full">
-              <div
-                className="rounded-full bg-indigo-600 text-xs leading-none text-center text-white py-1"
-                style={{ width: "75%" }}
-              >
-                75%
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            groundColor={`#000000`}
+            color={`#b1e1ff`}
+            intensity={1}
+          />
+          <Lobby />
+          <Prince
+            position={islandPosition}
+            scale={islandSca1e}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+        </Suspense>
+      </Canvas>
     </section>
   );
 }
